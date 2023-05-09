66f069ee6b9b08471ae8f126ac2421e6c5463c96
Mohammad Hasan Keramat J <ikeramat@protonmail.com>
2023-05-09 17:12:15 +0330

    Launcher3: recents: Disable lens perf if package is not installed
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Change-Id: I5e3322f9a19c1581bef475f141763178b908c5c0
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -178,6 +178,7 @@ public final class Utilities {
     public static final String KEY_SHOW_HOTSEAT_BG = "pref_show_hotseat_bg";
     public static final String KEY_ALLOW_WALLPAPER_ZOOMING = "pref_allow_wallpaper_zooming";
     public static final String KEY_STATUS_BAR = "pref_show_statusbar";
+    public static final String KEY_LENS = "pref_recents_lens";
 
     /**
      * Returns true if theme is dark.
