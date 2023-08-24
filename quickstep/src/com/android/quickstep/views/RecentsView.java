f781226e6c829a1e0e6c40703e3985830fcc0e39
nift4 <nift4@protonmail.com>
2023-08-24 14:16:43 +0200

    Launcher3: ensure to update cached thumbnail if we take new one
    This fixes a bug, which became obvious thanks to disabled live tile, but
    has already existed before, where recents jumps to using last snapshot
    instead of the correct/current one once you fling, because thumbnails
    were cached but the cache had not been updated in this codepath.
    
    Change-Id: Ie7afe56b6e0aecaae2510248c5887873800a0b51
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -5988,6 +5988,7 @@ public abstract class RecentsView<CONTAINER_TYPE extends Context & RecentsViewCo
                     mRecentsAnimationController.screenshotTask(container.getTask().key.id);
             TaskThumbnailViewDeprecated thumbnailView = container.getThumbnailViewDeprecated();
             if (td != null) {
+                container.getTask().thumbnail = td;
                 thumbnailView.setThumbnail(container.getTask(), td);
             } else {
                 thumbnailView.refresh();
