3d9e132daed836224817bdd2bc97d5e0f8d28c40
Mohammad Hasan Keramat J <ikeramat@libremobileos.com>
2024-04-09 11:21:54 +0000

    Launcher3: Restart on disabling overview action chips
    Change-Id: I045313b1828b8967701e87f9bbadb99d9e9aad50
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@libremobileos.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -184,6 +184,7 @@ public final class Utilities {
     public static final String KEY_ALLOW_WALLPAPER_ZOOMING = "pref_allow_wallpaper_zooming";
     public static final String KEY_STATUS_BAR = "pref_show_statusbar";
     public static final String KEY_LENS = "pref_recents_lens";
+    public static final String KEY_RECENTS_CHIPS = "pref_recents_chips";
 
     /**
      * Returns true if theme is dark.
